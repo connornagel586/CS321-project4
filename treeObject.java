023cc0fbff10eba88b0ad26c0951e9f57fc05362
Connor Nagel <connornagel@u.boisestate.edu>
2016-11-18 16:45:37 -0700

    completed treeObject

--- treeObject.java
+++ treeObject.java
@@ -1,11 +1,13 @@
-public class TreeObject {
+public class TreeObject implements comparable{
 
 	private long key = 0;
+	private int freq = 0;
 
 	public TreeObject(long key){
 		this.key = key;
 	}
 
+	@Override
 	public int compareTo(long key){
 		if(this.key == key){
 			return 0;
@@ -16,6 +18,26 @@ public class TreeObject {
 		if(this.key > key){
 			return 1;
 		}
+	}
+	public boolean equals(long key){
+		if(this.key == key){
+			return true;
+		}
+
+		return false;
+	}
+
+	public int getFreq(){
+		return this.freq;
+	}
+
+	public int increaseFreq(){
+	return freq++;
+	}
+	
+	@Override
+	public String toString(){
+		return key.toString();
 	}
 
 }
