a1073e54a9ab742b4e21b58990a1ddd1abbc2875
Connor Nagel <connornagel@u.boisestate.edu>
2016-11-17 18:48:07 -0700

    further work on skeletons for createBTree and treeObject

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -3,18 +3,31 @@ public class GeneBankCreateBTree{
 
 
 
-public static void main (String[] args){
+	public static void main (String[] args){
+
+		int degree, seqLength, cacheSize, usesCache = 0;
+		int debug = -1;
+		String fileName = "";
+	
 
 
 
+	}
 
 
 
-}
+	public void setVariables(){
 
+		usesCache = Integer.parseInt(args[0]);
+		degree = Integer.parseInt(args[1]);
+		fileName = args[2];
+		seqLength = Integer.parseInt(args[3]);
 
-
-public void CreateBTree(){}
-
-
+		if(args[4] != null){
+			cacheSize = Integer.parseInt(args[4]);
+		}
+		if(args[5] != null){
+			debug = Integer.parseInt(args[5]);
+		}
+	}
 }
--- treeObject.java
+++ treeObject.java
@@ -4,10 +4,18 @@ public class TreeObject {
 
 	public TreeObject(long key){
 		this.key = key;
-}
+	}
 
-public int compareTo(){
-
-}
+	public int compareTo(long key){
+		if(this.key == key){
+			return 0;
+		}
+		if(this.key < key){
+			return -1;
+		}
+		if(this.key > key){
+			return 1;
+		}
+	}
 
 }
