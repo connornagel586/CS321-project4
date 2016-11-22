b1353ce0e4fb78e48fcad03d9c805e323746a218
timadams-r <tim.adams.r@gmail.com>
2016-11-22 12:02:08 -0700

    Update BTree.java

--- BTree.java
+++ BTree.java
@@ -16,12 +16,12 @@ public class BTree<T> {
 	private void InsertNode(){
 
 		BTreeNode<T> r = root;
-		if (r.numKeys() == 2t - 1){
+		if (r.numKeys == 2 * degree - 1){
 			// uh-oh, the root is full, we have to split it
 			s = allocate-node ();
 			root = s; 	// new root node
-			s.isLeaf() = False; // will have some children
-			s.numKeys() = 0;	// for now
+			s.isLeaf = false; // will have some children
+			s.numKeys = 0;	// for now
 			s.childPointer[1] = r; // child is the old root node
 			SplitNode(s, 1, r); // r is split
 			InsertNodeNonFull(s, k); // s is clearly not full
@@ -34,9 +34,9 @@ public class BTree<T> {
 
 
 
-		int i = x.numKeys();
+		int i = x.numKeys;
 
-		if (x.isLeaf()){
+		if (x.isLeaf){
 
 			// shift everything over to the "right" up to the
 			// point where the new key k should go
@@ -66,9 +66,9 @@ public class BTree<T> {
 			// to be true, then read in that child node
 
 			i++;
-			Disk-Read (x.childPointer[i]);
+			Disk-Read(x.childPointer[i]);
 		}
-		if (x.childPointer[i].numKeys() = 2(degree) - 1){
+		if (x.childPointer[i].numKeys = 2 * degree - 1){
 
 			// this child node is full, split the node
 
@@ -91,57 +91,39 @@ public class BTree<T> {
 	private void SplitNode() {
 		/*		B-Tree-Split-Child (x, i, y)
 				z = allocate-node ()
-
 		// new node is a leaf if old node was 
-
 		leaf[z] = leaf[y]
-
 		// we since y is full, the new node must have t-1 keys
-
 		n[z] = t - 1
-
 		// copy over the "right half" of y into z
-
 		for j in 1..t-1 do
 		keyj[z] = keyj+t[y]
 		end for
-
 		// copy over the child pointers if y isn't a leaf
-
 		if not leaf[y] then
 		for j in 1..t do
 		cj[z] = cj+t[y]
 		end for
 		end if
-
 		// having "chopped off" the right half of y, it now has t-1 keys
-
 		n[y] = t - 1
-
 		// shift everything in x over from i+1, then stick the new child in x;
 		// y will half its former self as ci[x] and z will 	
 		// be the other half as ci+1[x]
-
 		for j in n[x]+1 downto i+1 do
 		cj+1[x] = cj[x]
 		end for
 		ci+1 = z
-
 		// the keys have to be shifted over as well...
-
 		for j in n[x] downto i do
 		keyj+1[x] = keyj[x]
 		end for
-
 		// ...to accomodate the new key we're bringing in from the middle 
 		// of y (if you're wondering, since (t-1) + (t-1) = 2t-2, where 
 		// the other key went, its coming into x)
-
 		keyi[x] = keyt[y]
 		n[x]++
-
 		// write everything out to disk
-
 		Disk-Write (y)
 		Disk-Write (z)
 		Disk-Write (x)*/
