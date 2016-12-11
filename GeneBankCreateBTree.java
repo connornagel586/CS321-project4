2e73b133c79f93542b478193cc22e68ca3c00395
timadams-r <tim.adams.r@gmail.com>
2016-12-11 13:31:13 -0700

    Update GeneBankCreateBTree.java

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -8,20 +8,15 @@ import java.util.StringTokenizer;
 public class GeneBankCreateBTree {
 	int debugLevel = -1, cacheSize, degree = -1, sequenceLength = 0;
 	boolean useCache = false;
-
-	public enum States {
-		START, SEQUENCE, UNKNOWN_CHARS, IN_SEQUENCE, END_SEQUENCE, END
-	}
-
-	public GeneBankCreateBTree() {
-		this.degree = degree;
-		this.sequenceLength = sequenceLength;
-		this.cacheSize = cacheSize;
-		this.debugLevel = debugLevel;
-		this.useCache = useCache;
-	}
-
 	private File file;
+	KeyMaker genKey;
+	
+
+	public GeneBankCreateBTree()throws IOException {
+		genKey = new KeyMaker(file);
+	}
+
+	
 
 	public File getFile() {
 		return this.file;
@@ -63,7 +58,7 @@ public class GeneBankCreateBTree {
 		this.sequenceLength = sequenceLength;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		GeneBankCreateBTree create = new GeneBankCreateBTree();
 
@@ -131,6 +126,18 @@ public class GeneBankCreateBTree {
 					System.exit(0);
 				}
 			}
+			
+		
+			BTree tree = new BTree(create.getDegree(), create.getFile());
+			KeyMaker genKey = new KeyMaker(create.getFile());
+			TreeObject o = new TreeObject(genKey.getNextKey());
+			 try {
+					 tree.insertNode(o);
+					
+					 } catch (IOException e) {
+					 e.printStackTrace();
+					 }
+			
 
 			//
 			// while (scan.hasNextLine()) {
@@ -212,6 +219,8 @@ public class GeneBankCreateBTree {
 				// File dump = new File("dump");
 				// tree.debugPrintIOT(dump);
 			}
+			
+			
 
 		} catch (NumberFormatException e) {
 			printUsage();
@@ -221,141 +230,11 @@ public class GeneBankCreateBTree {
 		}
 	}
 
-	class KeyMaker {
-		States state = States.START;
-		Scanner scan;
-		BTree tree;
-		File file;
-		String filename;
-		String line;
-		String key;
-
-		public KeyMaker(GeneBankCreateBTree create) throws IOException {
-			filename = (file.getName() + ".btree.data." + sequenceLength + "." + degree);
-			file = create.getFile();
-			tree = new BTree(degree, file);
-			scan = new Scanner(file);
-
-		}
-
-		public long getNextKey() throws Exception {
-			switch (state) {
-
-			case START: {
-				while (state == States.START) {
-
-					line = scan.nextLine();
-
-					if (line == null) {
-						state = States.END;
-
-					} else if (line.contains("ORIGIN")) {
-						state = States.SEQUENCE;
-					}
-				}
-			}
-			case SEQUENCE: {
-
-				while (state == States.IN_SEQUENCE) {
-
-					String ch = scan.next();
-					if (ch.contains("/")) {
-						state = States.END_SEQUENCE;
-						break;
-					} else if (ch.contains("n")) {
-
-						key = "";
-						state = States.UNKNOWN_CHARS;
-						break;
-					} else if (ch.contains("a") || ch.contains("t")
-							|| ch.contains("c") || ch.contains("g")) {
-						if (key.length() == sequenceLength) {
-							key = key.substring(1, sequenceLength);
-							key += ch;
-							return encode(key);
-						} else {
-
-							key += ch;
-							if (key.length() == sequenceLength) {
-
-								return encode(key);
-							}
-						}
-					}
-
-				}
-			}
-			case IN_SEQUENCE: {
-
-				while (state == States.IN_SEQUENCE) {
-
-					String ch = scan.next();
-					if (ch.contains("/")) {
-
-						state = States.END_SEQUENCE;
-					} else if (ch.contains("a") || ch.contains("t")
-							|| ch.contains("c") || ch.contains("g")) {
-						key += ch;
-						state = States.SEQUENCE;
-					}
-				}
-				break;
-
-			}
-
-			case END_SEQUENCE: {
-
-				line = scan.nextLine();
-				if (line != null) {
-
-					line.trim();
-					if (line.equals("ORIGIN")) {
-
-						state = States.SEQUENCE;
-						break;
-					}
-				}else{
-					
-					state = States.END;
-					break;
-				}
-			}
-
-			}
-			return 0; //for now
-		}
-	}
-
-	public long encode(String seq) throws Exception {
-		if (seq.length() != sequenceLength) {
-			throw new Exception("String of length " + seq.length()
-					+ " was passed to btree with sequenceLenght of "
-					+ sequenceLength);
-		}
-		long sequence = 0;
-		for (int i = 0; i < seq.length(); i++) {
-			sequence = sequence << 2;
-			char c = seq.charAt(i);
-			if (c == 'a' || c == 'A') {
-				sequence = sequence | 0x0L;
-			} else if (c == 't' || c == 'T') {
-				sequence = sequence | 0x3L;
-			} else if (c == 'c' || c == 'C') {
-				sequence = sequence | 0x1L;
-			} else if (c == 'g' || c == 'G') {
-				sequence = sequence | 0x2L;
-			} else {
-				throw new Exception("Unexpected character: " + c);
-			}
-		}
-		return sequence;
-	}
+	
+	
 
 	private static void printUsage() {
-		System.err
-				.println("GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> "
-						+ "<sequence length> 1<=k<=31>"
-						+ "[<cache size>] [<debug level>]"
-						+ "[<debug level>]\n");
+		System.err.println("GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> "
+				+ "<sequence length> 1<=k<=31>" + "[<cache size>] [<debug level>]" + "[<debug level>]\n");
 	}
 }
